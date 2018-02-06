1af02c0aa03f5443f269449f8274fa38f38248fa
Emily Jafar Movahhed <Emily@Emilys-MacBook-Air.local>
2018-02-06 15:10:45 +0000

    TheYumDiaries

--- TheYumDiaries2/app/src/main/java/com/example/android/theyumdiaries/LogIn.java
+++ TheYumDiaries2/app/src/main/java/com/example/android/theyumdiaries/LogIn.java
@@ -1,29 +1,38 @@
 package com.example.android.theyumdiaries;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class LogIn extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.log_in);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+        Button GoToNewActivity;
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.log_in);
+
+            GoToNewActivity = (Button)findViewById(R.id.button1);
+
+            GoToNewActivity.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+
+                    // Intent code for open new activity through intent.
+
+                    Intent intent = new Intent(LogIn.this, SignUp.class);
+                    startActivity(intent);
+
+                }
+            });
+        }
             }
-        });
-    }
 
-}
+
