cf60ee0356ff69dee4cb1f206be82b06da5c6309
guijcr <32732246+guijcr@users.noreply.github.com>
2018-02-08 16:47:41 +0000

    Update of my profile page
    Made the buttons a bit smaller since they were too big. Change one of the textbox for a spinner and added another spinner. This spinners will be one for the user to choose their dietary and other for the user to choose if they want their profile has public or private

--- TheYumDiaries2/app/src/main/java/com/example/android/theyumdiaries/profile_page.java
+++ TheYumDiaries2/app/src/main/java/com/example/android/theyumdiaries/profile_page.java
@@ -3,18 +3,23 @@ package com.example.android.theyumdiaries;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CursorAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class profile_page extends AppCompatActivity {
-    TextView Email, Dietary;
+    Spinner spinner;
+    CursorAdapter adapter;
+    TextView Email;
     Button save, edit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile_page);
         Email=(TextView) findViewById(R.id.text);
-        Dietary=(TextView) findViewById(R.id.txt2);
+
         save=(Button) findViewById(R.id.button);
         edit=(Button) findViewById(R.id.button2);
 
