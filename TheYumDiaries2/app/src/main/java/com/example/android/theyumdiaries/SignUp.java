0da36d41c321f7033aadd0dec1fa54a4c37d3490
Emily Jafar Movahhed <Emily@emilys-air.home>
2018-02-27 18:29:35 +0000

    sign up page
    pop up if two passwords dont match up on sign up page

--- TheYumDiaries2/app/src/main/java/com/example/android/theyumdiaries/SignUp.java
+++ TheYumDiaries2/app/src/main/java/com/example/android/theyumdiaries/SignUp.java
@@ -2,6 +2,11 @@ package com.example.android.theyumdiaries;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
 
 public class SignUp extends AppCompatActivity {
 
@@ -9,5 +14,37 @@ public class SignUp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sign_up);
+    }
+
+    public void onSignupClick(View v)
+    {
+            if (v.getId() == R.id.buttonSignup)
+            {
+                EditText firstname = (EditText) findViewById (R.id.ptFName);
+                EditText surname = (EditText) findViewById (R.id.ptSurname);
+                EditText email = (EditText) findViewById (R.id.ptEmail);
+                EditText password = (EditText) findViewById (R.id.signupPassword);
+                EditText confirmpassword = (EditText) findViewById (R.id.signupConfirmPW);
+
+                String firstnamestr = firstname.getText().toString();
+                String surnamestr = surname.getText().toString();
+                String emailstr = email.getText().toString();
+                String passwordstr = password.getText().toString();
+                String confirmpasswordstr = confirmpassword.getText().toString();
+
+                if (!passwordstr.equals(confirmpasswordstr))
+                {
+                    Toast pass = Toast.makeText(SignUp.this, "Passwords don't match!", Toast.LENGTH_SHORT);
+                    pass.show();
+                }
+
+
+
+
+
+
+
+
+            }
     }
 }
